899dfb65400d37cb9b4e0bcd7706c3c63ee010bc
Harley <harley.veillette@gmail.com>
2020-01-09 22:04:58 -0400

    Challenge Done

--- 1 First Apps/Homework01.1/app/src/main/java/work/nbcc/mydiceroller/MainActivity.kt
+++ 1 First Apps/Homework01.1/app/src/main/java/work/nbcc/mydiceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +15,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
+        var countButton: Button = findViewById(R.id.countUp_button)
+        countButton.setOnClickListener{Count()}
     }
 
     private fun rollDice(){
         var randomInt = Random().nextInt(6) + 1
+
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         var resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun Count(){
+        if(result_text.text == "Hello World"){
+            result_text.text = "1"
+        }
+        else{
+            var countup = result_text.text.toString().toInt()
+
+            if(countup < 6){
+                countup++
+                result_text.text = countup.toString()
+            }
+        }
     }
 }
