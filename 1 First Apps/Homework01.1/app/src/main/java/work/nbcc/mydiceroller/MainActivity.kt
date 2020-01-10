ffedb0ac55bd17cb71f9bfbc832e4fc71ff9e3d2
Harley <harley.veillette@gmail.com>
2020-01-09 21:27:19 -0400

    Adding new code before code challenge

--- 1 First Apps/Homework01.1/app/src/main/java/work/nbcc/mydiceroller/MainActivity.kt
+++ 1 First Apps/Homework01.1/app/src/main/java/work/nbcc/mydiceroller/MainActivity.kt
@@ -2,11 +2,24 @@ package work.nbcc.mydiceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{rollDice()}
+    }
+
+    private fun rollDice(){
+        var randomInt = Random().nextInt(6) + 1
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        var resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
